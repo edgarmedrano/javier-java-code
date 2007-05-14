6e1886bccd39c3915bb79301876598739dfc6402
Edgar Medrano <Edgar Medrano@localhost>
2007-05-13 23:17:06 -0500

    Javier20070513

--- src/org/javier/orderly/AGIHandler.java
+++ src/org/javier/orderly/AGIHandler.java
@@ -10,30 +10,26 @@
  */
 package org.javier.orderly;
 
+import static org.javier.jacob.OleAutomation.createActiveXObject;
+import static org.javier.jacob.SAPI.SpeechAudioFormatType.*;
+import static org.javier.jacob.SAPI.SpeechStreamFileMode.*;
+import static org.javier.jacob.SAPI.SpeechVoiceSpeakFlags.*;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-
 import org.javier.browser.Javier;
-import org.javier.browser.event.ErrorListener;
 import org.javier.browser.event.JavierListener;
 import org.javier.browser.event.OutputListener;
-import org.javier.browser.handlers.ConsoleErrorHandler;
-import org.javier.browser.handlers.ConsoleInputHandler;
-import org.javier.browser.handlers.ConsoleLogHandler;
-import org.javier.browser.handlers.ConsoleOutputHandler;
 import org.javier.browser.handlers.InputHandler;
 import org.javier.browser.handlers.MSXMLHTTPNetworkHandler;
-import org.javier.browser.handlers.SAPIOutputHandler;
 import org.javier.browser.handlers.StreamLogHandler;
+import org.javier.jacob.SAPI.ISpeechObjectTokens;
+import org.javier.jacob.SAPI.SpFileStream;
+import org.javier.jacob.SAPI.SpVoice;
 
 import com.orderlysoftware.orderlycalls.asterisk.agi.AGIConnection;
 import com.orderlysoftware.orderlycalls.asterisk.agi.AGIReusableProcessor;
-import com.orderlysoftware.orderlycalls.asterisk.agi.AGIServer;
-import com.orderlysoftware.orderlycalls.asterisk.agi.AGISettings;
-import com.orderlysoftware.orderlycalls.base.Settings;
 
 /**
  * AGI Input/Output handler
@@ -47,18 +43,38 @@ public class AGIHandler
 
 	private AGIConnection agi;
 	private Javier javier;
-	private Method command;
-	private Object commandArgs[];
-	private Object commandResult; 
+	private Thread agiThread;
+	private Thread javierThread;
+	private volatile boolean javierIsWaiting;
+	private boolean exitWaitLoop;
+	private static int file_index;
+	private SpVoice ttsVoice;
+	private String buffer = "";
 
+	private static synchronized int getFileIndex() {
+		return ++file_index;
+	}
+	
 	/* (non-Javadoc)
 	 * @see com.orderlysoftware.orderlycalls.asterisk.agi.AGIProcessor#processCall(com.orderlysoftware.orderlycalls.asterisk.agi.AGIConnection)
 	 */
 	public void processCall(AGIConnection agi) throws IOException {
 		this.agi = agi;
-		HashMap properties = agi.getAGIProperties();
+		//HashMap properties = agi.getAGIProperties();
+		
+		ttsVoice = (SpVoice) createActiveXObject(SpVoice.class);
+		ISpeechObjectTokens voices = ttsVoice.GetVoices();
+		
+		for(int i = 0; i < voices.Count(); i++) {
+			if(voices.Item(i).GetDescription().indexOf("Rosa") >= 0) {
+				ttsVoice.setVoice(voices.Item(i));
+				break;
+			}
+		}
 		
 		javier = new Javier(this,new MSXMLHTTPNetworkHandler());
+		javier.addJavierListener(this);
+		javier.addOutputListener(this);
 		
 		try {
 			javier.addLogListener(new StreamLogHandler("Javier.log"));
@@ -66,30 +82,29 @@ public class AGIHandler
 			e.printStackTrace();
 		}
 	
-		javier.mainLoop("http://localhost/sictel.php");
+		agiThread = Thread.currentThread();
+		javierThread = new Thread(new Runnable() {
+				public void run() {
+					javier.mainLoop("http://localhost/sictel.php");
+				}
+			});
 		
+		javierThread.start();
 		for(;;) {
-			if(command != null) {
+			if(javierIsWaiting) {
+				javierThread.notify();
 				try {
-					commandResult = command.invoke(agi, commandArgs);
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					e.printStackTrace();
-				}
-				while(commandResult != null) {
-					agi.noop();
-				}
-			} else {
-				if(command == null) {
-					agi.exec("WAIT", "1");
+					agiThread.wait();
+				} catch (InterruptedException e) {
+					exitWaitLoop = true;
 				}
 			}
+			if(exitWaitLoop) {
+				break;
+			}
+			agi.exec("WAIT", "1");
 		}
-		
-		
+		System.out.println("Succesfully end!");
 	}
 
 	/**
@@ -99,8 +114,7 @@ public class AGIHandler
 	 *            the end code
 	 */
 	public void excecutionEnded(int endCode) {
-		// TODO Auto-generated method stub
-		
+		exitWaitLoop = true;
 	}
 
 	/**
@@ -111,7 +125,6 @@ public class AGIHandler
 	 */
 	public void loadStateChanged(int readyState) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	/**
@@ -122,7 +135,6 @@ public class AGIHandler
 	 */
 	public void urlChanged(String url) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	/**
@@ -136,8 +148,30 @@ public class AGIHandler
 	 * @return the input
 	 */
 	public String getInput(String text, String value) {
-		// TODO Auto-generated method stub
-		return null;
+		String result = value;
+		waitForAGI();
+		try {
+			buffer += agi.getData("beep", 60000);
+			result = buffer;
+			buffer = "";
+		} catch (IOException e) {
+			exitWaitLoop = true;
+		}
+		agiThread.notify();
+		return result;
+	}
+
+	/**
+	 * Wait for AGI thread to be ready.
+	 */
+	private void waitForAGI() {
+		try {
+			javierIsWaiting = true;
+			javierThread.wait();
+		} catch (InterruptedException e) {
+			// do nothing
+		}
+		javierIsWaiting = false;
 	}
 
 	/**
@@ -147,24 +181,89 @@ public class AGIHandler
 	 *            the text
 	 */
 	public void addText(String text) {
-		// TODO Auto-generated method stub
+		String file = textToWav(text);
+		int result = 0;
 		
+		if(file == null) {
+			exitWaitLoop = true;
+		} else {
+			waitForAGI();
+			try {
+				result = agi.streamFile(file,"0123456789");
+			} catch (IOException e) {
+				exitWaitLoop = true;
+			}
+			agiThread.notify();
+		}
+		
+		if(result > 0) {
+			buffer += new String(new char[] { (char)result });
+		}
+	}
+
+	private String textToWav(String text) {
+		SpFileStream spfs = (SpFileStream) createActiveXObject(SpFileStream.class);
+		String fileName;
+		String wavPath;
+		String gsmPath;
+		File wavFile;
+		File gsmFile;
+		
+		/********************************************************
+		Implement caching here
+		*********************************************************/
+		
+		do {
+			fileName = "javier" + getFileIndex();
+			wavPath = "" + fileName + ".wav";
+			gsmPath = "C:\\cygroot\\asterisk\\var\\lib\\sounds" + fileName + ".gsm";
+			wavFile = new File(wavPath);
+			gsmFile = new File(gsmPath);
+		} while(wavFile.exists() || gsmFile.exists());
+		
+		spfs.getFormat().setType(SAFT8kHz8BitMono);
+		spfs.Open(wavPath, SSFMCreateForWrite, false);
+		
+		ttsVoice.setAllowAudioOutputFormatChangesOnNextSet(false);
+		ttsVoice.setAudioOutputStream(spfs);
+
+		ttsVoice.Speak(text, SVSFDefault);
+		
+		spfs.Close();
+		
+		ProcessBuilder soxProc = new ProcessBuilder("C:\\cygroot\\asterisk\\var\\lib\\sounds\\sox",wavPath,gsmPath);
+		try {
+			soxProc.start();
+		} catch (IOException e) {
+			fileName = null;
+			e.printStackTrace();
+		}
+		
+		while(!gsmFile.exists()) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		wavFile.delete();
+		
+		return fileName;
 	}
 
 	/**
 	 * Clear text.
 	 */
 	public void clearText() {
-		// TODO Auto-generated method stub
-		
+		// do nothing
 	}
 
 	/**
 	 * Wait until done.
 	 */
 	public void waitUntilDone() {
-		// TODO Auto-generated method stub
-		
+		// do nothing
 	}
 
 }
